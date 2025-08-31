4984dbf329a30cf0fd190b4e9c0febeea3fbe0e8
lgudge <lgudge@mail.valenciacollege.edu>
2025-08-31 17:41:51 -0400

    Initial Commit

---
+++ Variables.py
@@ -0,0 +1,34 @@
+# calculations for the student using pyton
+
+# Result 1: 5 Multiplied by 6
+result1 = 5 * 6
+print("5 multiplied by 6 is")
+print("the answer to your query is:", result1)
+
+# Result 2: 8 divided by 3
+result2 = 8 / 3
+print("\n8 divided by 3 is")
+print("the answer to your query is:", format(result2, ".8f"))
+
+# Result 3: 8 divided by 3 without the remainder
+result3 = 8 // 3
+print("\n8 divided by 3 is")
+print("the answer to your query is:", result3)
+
+# Result 4: 8 modulo 3 (remainder only)
+result4 = 8 % 3
+print("\n8 modulo 3 (remainder) is")
+print("the answer to your query is:", result4)
+
+# Result 5: 4200000 add 23090
+result5 = 4200000 + 23090
+print("\n4200000 add 23090 is")
+print("the answer to your query is:", result5)
+
+# Result 6: 4200000 subtract 7633
+result6 = 4200000 - 7633
+print("\n4200000 sub 76330 is")
+print("the answer to your query is:", result6)
+
+# Last line: Print my name
+print("\nProgram written by: Leon Gudge")
